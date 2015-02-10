12de78a9485d969dd0da9c4346752c930e05549a
Wes Okuhara <wokuhara@ucsd.edu>
2015-02-10 15:17:20 -0800

    added link to settings section of the digest page. Implemented smooth scroll functionality when link in navbar is clicked.

--- public/js/weyaka.js
+++ public/js/weyaka.js
@@ -16,3 +16,43 @@ $('.nav a').on('click', function(){
     	$('.navbar-collapse').collapse('hide');
     }
 });
+
+/* When links in the navbar on the digest page take you to the
+ * digest page, scroll to the section 
+ */
+$('.digest-link a').click(function(event) {
+	event.preventDefault(); //do not perform href action
+
+	$('html, body').scrollTo(this.hash, this.hash);
+	$(this).hover(false);
+});
+
+
+
+/*
+$(document).ready(function() {
+	$('#traffic').hover(function() {
+		$('#traffic-link').addClass('active');
+	}, function() {
+		$('#traffic-link').removeClass('active');
+	});
+
+	$('#weather').hover(function() {
+		$('#weather-link').addClass('active');
+	}, function() {
+		$('#weather-link').removeClass('active');
+	});
+
+	$('#notes').hover(function() {
+		$('#notes-link').addClass('active');
+	}, function() {
+		$('#notes-link').removeClass('active');
+	});
+
+	$('#schedule').hover(function() {
+		$('#schedule-link').addClass('active');
+	}, function() {
+		$('#schedule-link').removeClass('active');
+	});
+});
+*/
