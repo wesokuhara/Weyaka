8918faf5dbfab622c76a99a5c576de8818319e7e
vagrant <vagrant@precise32.(none)>
2015-02-12 02:07:17 +0000

    button disappears

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -11,33 +11,21 @@
 
     
 
-      var clientId = '897711787704-888q3u8rs86thukmam35s6eai6qa626h.apps.googleusercontent.com';
+      var clientId = '897711787704-gohcd3g1uffe71j4ugven87hjnuqjgub.apps.googleusercontent.com';
       var authorizationUrlBase = 'https://accounts.google.com/o/oauth2/auth';
-      var redirect = 'https://weyaka.heroku.com/digest';
+      var redirect = 'https://weyaka.herokuapp.com/digest';
       var scopes = 'https://www.googleapis.com/auth/calendar';
 
 
 
-      function startAuth(){
+      function startAuth(event){
       	var url = authorizationUrlBase;
       	url += '?response_type=code'
       		+ '&redirect_uri='+encodeURIComponent(redirect)
       		+ '&client_id=' +encodeURIComponent(clientId)
       		+ '&scope=' + encodeURIComponent(scopes);
-
-      		
-
-
-          console.log("im here");
+   
+   
       		window.location.href=url;
 
-      }
-
-
-      function checkAuth() {
-      	console.log("ssew");
-        alert(gapi.auth.getToken());
-      
-      }
-
-        
+   }
