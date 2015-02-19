5618b8267c18baa2ad1f48b5bff32f75451c8f35
Wes Okuhara <wesokuhara@yahoo.com>
2015-02-19 14:06:56 -0800

    fixed problems with calendar.js

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,19 +1,17 @@
 
-
-var events = require("../../events.json");
-
-$("#googleLink").click( function init(event){
- 	startAuth();
- 	console.log("Signing in to Google Calendar");
+$(document).ready( function() {
+  $("#googleLink").click( function init(event){
+   	startAuth();
+   	console.log("Signing in to Google Calendar");
+  });
 });
 
-var authorizationUrlBase = 'https://accounts.google.com/o/oauth2/auth';
-var redirect = 'https://www.google.com/calendar/render';
-var clientId = '897711787704-k22vac8ld5e5duptvne3fjjbtvgl01ss.apps.googleusercontent.com';
-var scopes = 'https://www.googleapis.com/auth/calendar';
 
-
-function startAuth(event){
+function startAuth (event) {
+  var authorizationUrlBase = 'https://accounts.google.com/o/oauth2/auth';
+  var redirect = 'https://www.google.com/calendar/render';
+  var clientId = '897711787704-k22vac8ld5e5duptvne3fjjbtvgl01ss.apps.googleusercontent.com';
+  var scopes = 'https://www.googleapis.com/auth/calendar';
 	var url = authorizationUrlBase;
 	url += '?response_type=token'
 		+ '&redirect_uri='+encodeURIComponent(redirect)
@@ -23,52 +21,44 @@ function startAuth(event){
 	window.open(url);
 }
 
-
-  function popup(event)
-   {
-
-    window.open("../../views/test.html");
-
-   }
-
-
-    var timecount=0;
-   function showEvent(event)
-
-   {
-
-   
-   var title = document.getElementById("calendarform").elements[0].value;
-   var location = document.getElementById("calendarform").elements[1].value;
-   var date = document.getElementById("calendarform").elements[2].value;
-   var time = document.getElementById("calendarform").elements[3].value;
-
-     window.alert("Event Successfully Added");
-    
-
-          
+function popup(event){
+  window.open("../../views/test.html");
+}
 
 
 
-        $("#currenteventlist").html(eventhtml);
+function showEvent(event) {
+  var timecount=0;
+ 
+ var title = document.getElementById("calendarform").elements[0].value;
+ var location = document.getElementById("calendarform").elements[1].value;
+ var date = document.getElementById("calendarform").elements[2].value;
+ var time = document.getElementById("calendarform").elements[3].value;
 
-        var storing = {
+ window.alert("Event Successfully Added");
+  
 
-          "title":document.getElementById("calendarform").elements[0].value,
-          "location": document.getElementById("calendarform").elements[1].value,
-          "date": document.getElementById("calendarform").elements[2].value,
-          "time": document.getElementById("calendarform").elements[3].value
-        }
-
-        //events["events"].push(storing);
-      //res.render("index", events);
-      //document.write(storing);
-
+        
 
 
 
+      $("#currenteventlist").html(eventhtml);
 
-   console.log(eventhtml);
+      var storing = {
+
+        "title":document.getElementById("calendarform").elements[0].value,
+        "location": document.getElementById("calendarform").elements[1].value,
+        "date": document.getElementById("calendarform").elements[2].value,
+        "time": document.getElementById("calendarform").elements[3].value
+      }
+
+      //events["events"].push(storing);
+    //res.render("index", events);
+    //document.write(storing);
+
+    console.log(storing);
+
+ console.log(eventhtml);
 }
 
    /**
