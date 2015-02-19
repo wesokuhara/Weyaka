f2447e55a51b3b91154185fff984d780cbe718d9
vagrant <vagrant@precise32.(none)>
2015-02-19 20:00:14 +0000

    calendar json works

--- routes/digest.js
+++ routes/digest.js
@@ -8,10 +8,12 @@
 
  //Load JSON data
 var data = require('../data.json');
+var events = require('../events.json');
+
 
 exports.view = function(req, res) {
 	console.log("# Loading digest page...");
 	console.log(data);
 	
-	res.render('digest', data);
+	res.render('digest', events);
 };
