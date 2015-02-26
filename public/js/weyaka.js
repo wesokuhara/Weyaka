dea2a7a05104d44b3c68dfcccb32bf1e66c806ae
Wes Okuhara <wesokuhara@yahoo.com>
2015-02-25 17:48:40 -0800

    added digest links to always display in navbar.  Consolidated Settings and Help into one page.:

--- public/js/weyaka.js
+++ public/js/weyaka.js
@@ -10,22 +10,17 @@
 
 $(document).ready( function() {
 
-	/* When a digest link is clicked, collapse the navbar dropdown
-	 * if it is open and then scroll to that section of the page
-	 */
+	/* When a digest link is clicked scroll to the section */
 	$('.digest-link').click( function (event) {
 
-		if ($('.navbar-toggle').css('display') != 'none'){
-	    	$('.navbar-collapse').collapse('hide');
-	    }
-
 		event.preventDefault();
+
 		$('html, body').scrollTo(this.hash, this.hash);
 	});
 
 	/* Function to confirm with the user they want to logout
 	 */
-	$('.logout-button').click( function() {
+	$('#logout-button').click( function() {
 		return confirm('Are you sure you want to logout?');
 	});
 });
