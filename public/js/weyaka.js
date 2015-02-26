8bdf4c36258e57070afd4487b82f3dcb422b29fe
Wes Okuhara <wesokuhara@yahoo.com>
2015-02-25 20:24:02 -0800

    added scroll functionality to highlight links in navbar

--- public/js/weyaka.js
+++ public/js/weyaka.js
@@ -23,4 +23,37 @@ $(document).ready( function() {
 	$('#logout-button').click( function() {
 		return confirm('Are you sure you want to logout?');
 	});
+
+	/* add active class to navbar digest links */
+	$(window).scroll( function() {
+
+		var scrollPosition = $(window).scrollTop();
+
+		$('.digest-link-icons ul li').each( function() {
+			var currentListElement = $(this);
+			var refElement = $(currentListElement.children().attr("href"));
+
+			if (refElement.position().top <= scrollPosition &&
+				refElement.position().top + refElement.height() > scrollPosition) {
+
+				//remove active class from all list items
+				$('.digest-link ul li').removeClass('active');
+				currentListElement.addClass('active');
+			}
+
+			else {
+				currentListElement.removeClass('active');
+			}
+		});
+	});
+
+	/* When a link is clicked, add the active class */
+	$('.nav li').on('click', function(){
+
+		//remove active class from all li elements
+	    $('.nav li').removeClass('active');
+
+	    //add active class to the li clicked
+	    $(this).addClass('active');
+	});
 });
